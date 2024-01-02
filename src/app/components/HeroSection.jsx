34260feb0ef240a14fada309708fde65a7a68b67
ikhsanfd1 <dilah80999@gmail.com>
2024-01-02 11:05:35 +0700

    updated 006

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -60,7 +60,7 @@ const HeroSection = () => {
           transition={{ duration: 0.5 }}
           className="col-span-4 place-self-center mt-4 lg:mt-0"
         >
-          <div className="rounded-full bg-[#231942] w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
+          <div className="rounded-full bg-gradient-to-r from-primary-400 to-secondary-600 w-[250px] h-[250px] lg:w-[400px] lg:h-[400px] relative">
             <Image
               src="/images/foto_cv_ai_new.png"
               alt="hero image"
