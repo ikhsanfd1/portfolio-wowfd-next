5f3e458c6b0f72914ea6e084adf9c39f1861560c
ikhsanfd1 <dilah80999@gmail.com>
2024-01-02 10:41:53 +0700

    updated 005

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="footer border z-10 border-t-[#231942] border-l-transparent border-r-transparent text-[#231942]">
       <div className="container p-12 flex justify-between">
-        <span>Wow FD</span>
+        <span>Ikhsan FD</span>
         <p className="text-[#231942]">All rights reserved.</p>
       </div>
     </footer>
